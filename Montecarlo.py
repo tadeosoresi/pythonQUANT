642d8ba28fabf2677061bd0e8c45e611a1e1f597
Tadeo <74320188+tadeosoresi@users.noreply.github.com>
2021-08-27 12:00:38 -0300

    montecarlo final

--- Montecarlo.py
+++ Montecarlo.py
@@ -55,50 +55,7 @@ def montecarlo(tickeryahoo, nombre):
     plt.show()
 
 montecarlo('ALUA.BA', 'Aluar Aluminio')
-
-def montecarlo2(tickeryahoo2, nombre2):
-    data = pd.DataFrame()
-    data[tickeryahoo2] = wb.DataReader(tickeryahoo2, data_source='yahoo', start='2018-01-01')['Adj Close']
-    
-    #Importamos ultima cotizacion
-    import pandas_datareader as pdr
-    data2 = pdr.get_data_yahoo('VALO.BA')
-    lastCoti = data2.loc[data2.index[-1], "Adj Close"]
-    
-    #Mu varianza deriva desviación
-    log_returns = np.log(1+ data.pct_change())
-    u = log_returns.mean()
-    var = log_returns.var()
-    drift = u - (0.5 * var)
-    stdev = log_returns.std()
-    
-    #Proyeccion tiempo y simulaciones
-    tiempo = 120
-    iteraciones = 100
-    
-    #Escenarios posibles beneficios fut
-    daily_returns = np.exp(drift.values + stdev.values * norm.ppf(np.random.rand(tiempo, iteraciones)))
-    
-    #Dato diario
-    di = data.iloc[-1]
-    price_list = np.zeros_like(daily_returns)
-    price_list[0] = di
-    
-    for t in range(1, tiempo):
-        price_list[t] = price_list[t - 1] * daily_returns[t]
-    
-   #Grafico
-    plt.figure(figsize=(14, 5))
-    plt.title("MonteCarlo proyectado a " + nombre2)
-    plt.ylabel("Precio")
-    plt.xlabel("Tiempo")
-    plt.plot(price_list)
-    plt.axhline(y=lastCoti, xmin=0, xmax=tiempo, color='red', linewidth=2.4, linestyle='--')
-    plt.style.use('dark_background')
-    log_returns.plot(figsize = (13.5, 4), title=("Retornos logaritmicos"))
-    plt.show()
-
-montecarlo2('VALO.BA', 'Grupo financiero Valores')
+montecarlo('VALO.BA', 'Grupo financiero Valores')
 
 
     
